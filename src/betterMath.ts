c297eec97ed9c5ecb1945df97ba88fead3146d02
AetherDim <74829006+AetherDim@users.noreply.github.com>
2021-06-28 21:55:37 +0200

    First bad sqrt

--- src/betterMath.ts
+++ src/betterMath.ts
@@ -3,3 +3,16 @@
 export function add(a: number, b: number) {
     return a + b;
 }
+
+export function sqrt(q: number) {
+    if(q < 0) {
+        return NaN
+    }
+
+    let x = q;
+    for(let i = 0; i < 7; i++) {
+        x = x - (x - q/x)/2;
+    }
+
+    return x;
+}
--- src/tests/tests.ts
+++ src/tests/tests.ts
@@ -1,4 +1,4 @@
-import { add } from "../betterMath";
+import { add, sqrt } from "../betterMath";
 
 
 QUnit.module('add');
@@ -6,3 +6,22 @@ QUnit.module('add');
 QUnit.test('add two numbers', assert => {
   assert.equal(add(1, 1), 2);
 });
+
+
+QUnit.module('sqrt')
+
+QUnit.test('sqrt', assert => {
+  assert.equal(sqrt(4), 2)
+  assert.equal(sqrt(5), 2.2360679775)
+  assert.equal(sqrt(28376478263784627835), 5326957693.072532)
+  assert.ok(isNaN(sqrt(-1)))
+})
+
+QUnit.test('sqrt benchmark', assert => {
+  for (let i = 0; i < 1_000_000; i++) {
+    sqrt(5)
+  }
+  assert.ok(true)
+})
+
+
